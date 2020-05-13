0851f16f7a180190e05c93f6cd7cbdcb86b48ecf
Barnaba <ciao@gmail.com>
2020-05-13 10:52:43 +0200

    versione1

---
+++ CasciaroBarnaba/src/CasciaroTest.java
@@ -0,0 +1,29 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CasciaroTest {
+
+	@Test
+	void testCasciaroMet() {
+		CasciaroClass c=new CasciaroClass();
+		int n=11;
+		int risAtteso=2187;
+		float risOttenuto=c.CasciaroMet(3, 7);
+	}
+	@Test
+	void testCasciaroMet2() {
+		CasciaroClass c=new CasciaroClass();
+		int n=0;
+		int risAtteso=0;
+		float risOttenuto=c.CasciaroMet(0, 1);
+	}
+	@Test
+	void testCasciaroMet3() {
+		CasciaroClass c=new CasciaroClass();
+		int n=0;
+		int risAtteso=1;
+		float risOttenuto=c.CasciaroMet(1, 0);
+	}
+
+}
